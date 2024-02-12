a64215c5954b5846922d837c57bff0d332e955e0
Shahriar Ahmmed <shahriarzemam28@gmail.com>
2024-02-12 22:28:28 +0600

    Footer Updated

--- src/Components/Home/Footer/Footer.jsx
+++ src/Components/Home/Footer/Footer.jsx
@@ -1,7 +1,11 @@
 const Footer = () => {
     return (
         <div>
-            
+            <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+                <aside>
+                    <p>Copyright © 2024 - All right reserved by ACME Industries Ltd</p>
+                </aside>
+            </footer>
         </div>
     );
 };
