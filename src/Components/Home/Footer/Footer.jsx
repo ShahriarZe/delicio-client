d3ced91d31a66cd13de4ce466b4b6c502327a78d
Shahriar Ahmmed <shahriarzemam28@gmail.com>
2024-02-16 22:35:48 +0600

    Cards updated

--- src/Components/Home/Footer/Footer.jsx
+++ src/Components/Home/Footer/Footer.jsx
@@ -1,3 +1,5 @@
+import watch from '../../../assets/wtc.webp'
+
 const Footer = () => {
     return (
         <div>
@@ -31,7 +33,7 @@ const Footer = () => {
 
         <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             <a href="#">
-                <img className="p-8 rounded-t-lg" src="/docs/images/products/apple-watch.png" alt="product image" />
+                <img className="p-8 rounded-t-lg" src={watch} alt="product image" />
             </a>
             <div className="px-5 pb-5">
                 <a href="#">
