1a2fa3e95fba48179166427d61445165e7080bcf
Shahriar Ahmmed <shahriarzemam28@gmail.com>
2024-02-19 22:01:21 +0600

    Banner Slider Added

--- src/Components/Home/Home/Home.jsx
+++ src/Components/Home/Home/Home.jsx
@@ -1,8 +1,9 @@
+import Banner from "../Banner/Banner";
+
 const Home = () => {
     return (
         <div>
-            h2
-            this is Home
+            <Banner></Banner>
         </div>
     );
 };
