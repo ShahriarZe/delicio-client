d99bdf4692d66949b5aec7c0804783b28a7d598b
Shahriar Ahmmed <shahriarzemam28@gmail.com>
2024-02-21 23:17:09 +0600

    Image Slider Edited

--- src/Components/Home/Banner/Banner.jsx
+++ src/Components/Home/Banner/Banner.jsx
@@ -28,8 +28,8 @@ const Banner = () => {
         modules={[Autoplay, Pagination, Navigation]}
         className="mySwiper"
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Image 1</SwiperSlide>
+        <SwiperSlide>Image 2</SwiperSlide>
         <SwiperSlide>Slide 3</SwiperSlide>
         <SwiperSlide>Slide 4</SwiperSlide>
         <SwiperSlide>Slide 5</SwiperSlide>
