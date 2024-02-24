8cbbba471c9bcd878acfb96ed6da68756a59db1f
Shahriar Ahmmed <shahriarzemam28@gmail.com>
2024-02-24 22:51:42 +0600

    Banner Image Added with Swiper

--- src/Components/Home/Banner/Banner.jsx
+++ src/Components/Home/Banner/Banner.jsx
@@ -1,3 +1,6 @@
+import banner1 from '../../../assets/Banner1.png'
+import banner2 from '../../../assets/Banner2.png'
+
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 
@@ -18,7 +21,7 @@ const Banner = () => {
         spaceBetween={30}
         centeredSlides={true}
         autoplay={{
-          delay: 2500,
+          delay: 3500,
           disableOnInteraction: false,
         }}
         pagination={{
@@ -28,15 +31,13 @@ const Banner = () => {
         modules={[Autoplay, Pagination, Navigation]}
         className="mySwiper"
       >
-        <SwiperSlide>Image 1</SwiperSlide>
-        <SwiperSlide>Image 2</SwiperSlide>
-        <SwiperSlide>Image 3</SwiperSlide>
-        <SwiperSlide>Image 4</SwiperSlide>
-        <SwiperSlide>Image 5</SwiperSlide>
-        <SwiperSlide>Image 6</SwiperSlide>
-        <SwiperSlide>Image 7</SwiperSlide>
-        <SwiperSlide>Image 8</SwiperSlide>
-        <SwiperSlide>Image 9</SwiperSlide>
+        <SwiperSlide>
+          <img src={banner1} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={banner2} alt="" />
+        </SwiperSlide>
+  
       </Swiper>
         </div>
     );
