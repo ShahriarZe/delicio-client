be3d7ffe2ebeb3ec0a1d32b35eb55cb5e68454d5
Shahriar Ahmmed <shahriarzemam28@gmail.com>
2024-02-25 23:01:10 +0600

    Banner Image Added with Swiper

--- src/Components/Home/Banner/Banner.jsx
+++ src/Components/Home/Banner/Banner.jsx
@@ -1,5 +1,7 @@
 import banner1 from '../../../assets/Banner1.png'
 import banner2 from '../../../assets/Banner2.png'
+import banner3 from '../../../assets/Banner3.png'
+import banner4 from '../../../assets/Banner4.png'
 
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -21,7 +23,7 @@ const Banner = () => {
         spaceBetween={30}
         centeredSlides={true}
         autoplay={{
-          delay: 3500,
+          delay: 4500,
           disableOnInteraction: false,
         }}
         pagination={{
@@ -34,8 +36,17 @@ const Banner = () => {
         <SwiperSlide>
           <img src={banner1} alt="" />
         </SwiperSlide>
+
         <SwiperSlide>
           <img src={banner2} alt="" />
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <img src={banner3} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+
+          <img src={banner4} alt="" />
         </SwiperSlide>
   
       </Swiper>
